8713228649cbdfb35240277039656839c2a63d64
ClumsyPotato <lake_bodom@kabelbw.de>
2018-11-27 19:11:18 +0100

    hmmm

--- build.sh
+++ build.sh
@@ -7,12 +7,15 @@ set -e
 cd resource-tutorial
 ./mvnw -Dmaven.test.skip=true clean install
 
-git config --global user.email "lake_bodom@kabelbw.de"
-git config --global user.name "ClumsyPotato"
+#git config --global user.email "lake_bodom@kabelbw.de"
+#git config --global user.name "ClumsyPotato"
 
-git add target/*
+#git add target/*
 
-git commit -m "updated"
+#git commit -m "updated"
+
+I
+
 
 ls target/
 pwd
