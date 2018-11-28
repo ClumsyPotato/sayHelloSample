8ae78c00e648d5762a82e8ad199bb6c74ee0bb77
ClumsyPotato <lake_bodom@kabelbw.de>
2018-11-28 18:59:08 +0100

    commit.sh

--- commit.sh
+++ commit.sh
@@ -13,7 +13,10 @@ git clone resource-tutorial updated-repo
 ls -a resource-tutorial
 ls -a updated-repo
 rm -r ./updated-repo/target
+ls -a
 cp -r ./target/ ./updated-repo/
+ls -a
+
 
 #clone git repo to another location to be able to push it
 git config --global user.email "lake_bodom@kabelbw.de"
